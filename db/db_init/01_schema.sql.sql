9083c68283bad2322dfb65e3597dd7fee6532299
Ron DeBenedetti <ron.debenedetti@praeses.com>
2024-01-16 18:44:51 -0600

    add created_at column to time entries

--- db/db_init/01_schema.sql.sql
+++ db/db_init/01_schema.sql.sql
@@ -13,11 +13,13 @@ CREATE TABLE IF NOT EXISTS time_tracking.time_entries (
     total_time BIGINT,
     note TEXT,
     day SMALLINT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     charge_code_id INTEGER NULL REFERENCES time_tracking.charge_codes(id) ON DELETE SET NULL
 );
 
-CREATE TABLE time_tracking.time_entry_charge_code (
-    time_entry_id INTEGER REFERENCES time_tracking.time_entries(id) ON DELETE CASCADE,
-    charge_code_id INTEGER REFERENCES time_tracking.charge_codes(id) ON DELETE CASCADE,
-    PRIMARY KEY (time_entry_id, charge_code_id)
-);
+-- if you ever want time_entries to have many charge codes
+--CREATE TABLE time_tracking.time_entry_charge_code (
+--    time_entry_id INTEGER REFERENCES time_tracking.time_entries(id) ON DELETE CASCADE,
+--    charge_code_id INTEGER REFERENCES time_tracking.charge_codes(id) ON DELETE CASCADE,
+--    PRIMARY KEY (time_entry_id, charge_code_id)
+--);
